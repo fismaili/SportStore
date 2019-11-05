a81a66a4c63eee905b36b2239bd898dc42dc88ca
fismaili <45186231+fismaili@users.noreply.github.com>
2019-11-05 21:22:03 +0100

    SeedData.cs Adde3d and configuration made

--- Program.cs
+++ Program.cs
@@ -19,6 +19,6 @@ namespace SportStore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseDefaultServiceProvider(options=>options.ValidateScopes=false);
     }
 }
